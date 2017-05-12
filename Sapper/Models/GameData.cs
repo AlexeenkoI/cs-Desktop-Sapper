76a3b62f8dff38cac7c0ce4646310e34c30d45fc
barbeu <yuriy.barbeu@gmail.com>
2017-05-12 21:53:06 +0300

    Create model GameData.

---
+++ Sapper/Models/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sapper.Models
+{
+    class GameData
+    {
+        public int cUserId          { get; set; }
+        public int cGameLvl         { get; set; }
+        public int cGameType        { get; set; }
+        public TimeSpan cGameTime   { get; set; }
+        public DateTime cCreateTime { get; set; }
+    }
+}
