14309cd373ba4c3b39c26ed453ed0a4df2db0d9e
barbeu <yuriy.barbeu@gmail.com>
2017-05-12 22:44:06 +0300

    Added ScoreWindow.

---
+++ Sapper/UI/ScoreWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Sapper.Models;
+using System.Windows;
+
+namespace Sapper.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для ScoreWindow.xaml
+    /// </summary>
+    public partial class ScoreWindow : Window
+    {
+        public ScoreWindow(GameData data)
+        {
+            InitializeComponent();
+            InitializeLables(data);              
+        }
+
+        private void InitializeLables(GameData data)
+        {
+            lb_gamelvl.Content = data.cGameLvl;
+            lb_gameTime.Content = data.cGameTime;
+            lb_gameScore.Content = data.cGameScore;
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            // 1 - Model 'GameData' Generate JSON string
+            // 2 - JSON string to the server via HTTP POST 
+                        
+            Close();
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
