e319ff70b790b98f20e53b3a4091eec0c4eba5d8
barbeu <yuriy.barbeu@gmail.com>
2017-05-13 23:29:22 +0300

    Added class show systems messages.
    Use example -> Message.show( (int) Messages.AUTH_DATA_ERROR);

---
+++ Sapper/Src/Message.cs
@@ -0,0 +1,23 @@
+﻿using Sapper.Enums;
+using System.Windows;
+
+namespace Sapper.Src
+{
+    public static class Message
+    {
+        public static void show (int value)
+        {
+            Messages msg = (Messages)value;
+
+            switch(msg)
+            {                
+                case Messages.AUTH_DATA_ERROR:
+                    MessageBox.Show("Пароль или Логин введен неверно.");
+                    break;
+                case Messages.ERROR_CONNECTION_SERVER:
+                    MessageBox.Show("Ошибка соединения с сервером.");
+                    break;
+            }
+        }    
+    }
+}
