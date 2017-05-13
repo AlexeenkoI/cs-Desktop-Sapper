96726d3c6f22054d9685a7437e30a68931f575f6
barbeu <yuriy.barbeu@gmail.com>
2017-05-13 23:27:22 +0300

    Added enum systems messages.

---
+++ Sapper/Enums/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper.Enums
+{
+    public enum Messages
+    {
+        AUTH_DATA_ERROR = 1,
+        ERROR_CONNECTION_SERVER = 2
+    }
+}
