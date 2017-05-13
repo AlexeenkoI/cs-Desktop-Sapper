7838f199fc984c7e7d53d6932ee7d2dfaec5d9a8
Igor Alekseenko <fastedx@gmail.com>
2017-05-14 00:42:24 +0300

    Adding State and CellType Enums

---
+++ Sapper/Enums/CellType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper.Enums
+{
+    public enum CellType
+    {
+        Mine = 1,
+        EmptyField = 2,
+        NumericField = 3
+    }
+}
---
+++ Sapper/Enums/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper.Enums
+{
+    public enum State
+    {
+        Open = 1,
+        Close = 2,
+        Flag = 3,
+        Question = 4
+    }
+}
