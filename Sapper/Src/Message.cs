6657914106ba23703eb663a791f28fa09a325015
barbeu <yuriy.barbeu@gmail.com>
2017-05-15 22:17:08 +0300

    Added system messages.

--- Sapper/Src/Message.cs
+++ Sapper/Src/Message.cs
@@ -12,10 +12,22 @@ namespace Sapper.Src
             switch(msg)
             {                
                 case Messages.AUTH_DATA_ERROR:
-                    MessageBox.Show("Пароль или Логин введен неверно.");
+                    MessageBox.Show("Password or login is entered incorrectly");
                     break;
                 case Messages.ERROR_CONNECTION_SERVER:
-                    MessageBox.Show("Ошибка соединения с сервером.");
+                    MessageBox.Show("Error connecting to server");
+                    break;
+                case Messages.PASSWORDS_DO_NOT_MATCH:
+                    MessageBox.Show("Passwords do not match");
+                    break;
+                case Messages.EMPTY_LOGIN:
+                    MessageBox.Show("Login field is empty");
+                    break;
+                case Messages.EMPTY_PASSWORD:
+                    MessageBox.Show("Password field is empty");
+                    break;
+                case Messages.EMPTY_NAME:
+                    MessageBox.Show("Name field is empty");
                     break;
             }
         }    
