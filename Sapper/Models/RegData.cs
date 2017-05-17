81764c5a007df34c7d9e1c850fa9b8a4fe496391
Igor Alekseenko <fastedx@gmail.com>
2017-05-17 10:07:11 +0300

    added regisrationData model

---
+++ Sapper/Models/RegData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper.Models
+{
+    public class RegData
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Nickname { get; set; }
+        public DateTime regTime { get; set; }
+    }
+}
