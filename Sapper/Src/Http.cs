c17206ddb2316fec0da5ed482491fcb9f919bf11
Igor Alekseenko <fastedx@gmail.com>
2017-05-17 13:46:00 +0300

    adding first method for http class

---
+++ Sapper/Src/Http.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Sapper.Enums;
+using Sapper.Models;
+using System.Runtime.Serialization;
+namespace Sapper.Src
+{
+    public static class Http
+    {
+        
+        public static async void RegRequest(RegData regData)
+        {
+             string uri = "1.1.1.1/";
+            // string time = regData.regTime.ToString();
+            // var sendContent = new FormUrlEncodedContent(new[] {
+            //     new KeyValuePair<string, string>("login", regData.Name),
+            //     new KeyValuePair<string, string>("password", regData.Password),
+            //     new KeyValuePair<string, string>("nickName", regData.Nickname),
+            //     new KeyValuePair<string, string>("time",time)
+            // });
+            
+            string sendContent = json.generateRegJson(regData);  //to do
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(uri, sendContent);
+            if (response.IsSuccessStatusCode)
+            {
+                //some logic for user to show that he's succsessfully registered
+            }else
+            {
+                //some logic for user to show that something goes wrong
+                
+            }
+
+        }
+    }
+}
