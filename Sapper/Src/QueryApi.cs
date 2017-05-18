2d20c0e3a48929ae5cbd82107b9b32f80d53c744
barbeu <yuriy.barbeu@gmail.com>
2017-05-18 20:25:15 +0300

    Added query api to server.

---
+++ Sapper/Src/QueryApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper.Src
+{
+    public static class QueryApi
+    {
+        public static string SERVER = ConfigurationManager.AppSettings.GetValues("HttpServer").FirstOrDefault();
+        public static string PORT = ConfigurationManager.AppSettings.GetValues("Port").FirstOrDefault();
+        public static string QUERY_AUTH = ConfigurationManager.AppSettings.GetValues("QueryAuth").FirstOrDefault();
+        public static string QUERY_REGISTER = ConfigurationManager.AppSettings.GetValues("QueryRegister").FirstOrDefault();
+        public static string QUERY_SAVE_DATA = ConfigurationManager.AppSettings.GetValues("QuerySaveData").FirstOrDefault();
+        public static string QUERY_GET_DATA = ConfigurationManager.AppSettings.GetValues("QueryGetData").FirstOrDefault();
+    }
+}
