5d41c98351d4ac83d65355556efd46d9380e6fcb
Igor Alekseenko <fastedx@gmail.com>
2017-05-18 15:09:56 +0300

    Added authrequest method and regData model

---
+++ Sapper/Models/AuthData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper.Models
+{
+    public class AuthData
+    {
+        public int userId { get; set; }
+        public int nickName { get; set; }
+        public int Token { get; set; }
+    }
+}
--- Sapper/Src/Http.cs
+++ Sapper/Src/Http.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using Sapper.Enums;
 using Sapper.Models;
 using System.Runtime.Serialization;
+
+
 namespace Sapper.Src
 {
     public static class Http
     {
         
-        public static async void RegRequest(RegData regData)
+        public static async Task<bool> RegRequest(RegData regData)
         {
-            string uri = "1.1.1.1/";
+            
             // string time = regData.regTime.ToString();
             // var sendContent = new FormUrlEncodedContent(new[] {
             //     new KeyValuePair<string, string>("login", regData.Name),
@@ -22,20 +25,46 @@ namespace Sapper.Src
             //     new KeyValuePair<string, string>("nickName", regData.Nickname),
             //     new KeyValuePair<string, string>("time",time)
             // });
-           
-            var sendContent = json.generateRegJson(regData);  //to do class json
+            HttpContent r = new StringContent("adasd");
+            HttpContent sendContent = Json.generateRegJson(regData);  //to do class json
             var httpClient = new HttpClient();
-            var response = await httpClient.PostAsync(uri, sendContent);
+            
+            
+            var response = await httpClient.PostAsync(uri, sendContent); // uri will be switched for configurred baseaddr
             if (response.IsSuccessStatusCode)
             {
-                //some logic for user to show that he's succsessfully registered
+                return true;//some logic for user to show that he's succsessfully registered
                 
             }else
             {
                 //some logic for user to show that something goes wrong
+                return false;
                 
             }
 
+        }
+
+        public static async Task<AuthData> AuthRequest(string log, string pass)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic", 
+                Convert.ToBase64String(
+                Encoding.ASCII.GetBytes(
+                string.Format("{0}:{1}",log,pass))));
+            var response = await httpClient.GetAsync(uri); // uri will be switched for configurred baseaddr
+            
+            if (response.IsSuccessStatusCode)
+            {
+                AuthData constructedData = Json.parseAuthData(response);
+                //some logic for user to show that he's succsessfully registered
+                return constructedData;
+            }
+            else
+            {
+                return null;//some logic for user to show that something goes wrong
+
+            }
         }
     }
 }
