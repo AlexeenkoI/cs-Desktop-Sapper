b04dac7340fbcab7623b65dd86fe54989890c480
Igor Alekseenko <fastedx@gmail.com>
2017-05-29 17:00:47 +0300

    Add VisualCell presentation

---
+++ Sapper/Src/Elements/VisualCell.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Sapper.Src.Elements
+{
+    class VisualCell
+    {
+        Border border;
+        bool lmPressed;
+        bool rmPressed;
+
+        public VisualCell(double height, double width)
+        {
+            border = new Border();
+            border.Height = 30;//inc heigh
+            border.Width = 30;//inc width
+
+            border.BorderBrush = Brushes.Black;
+            border.BorderThickness = new Thickness(2);
+            border.Background = Brushes.Silver;
+
+            border.MouseEnter += mouse_in;
+            border.MouseLeave += mouse_leave;
+            border.MouseLeftButtonDown += mouse_down;
+
+            lmPressed = false;
+            rmPressed = false;
+        }
+
+
+        private void mouse_in(object sender, EventArgs e)
+        {
+            Border eventedR = (Border)sender;
+            eventedR.BorderBrush = Brushes.Black;
+
+
+        }
+
+        private void mouse_leave(object sender, EventArgs e)
+        {
+            Border eventedR = (Border)sender;
+            eventedR.BorderBrush = Brushes.Gray;
+        }
+
+        private void mouse_down(object sender, EventArgs e)
+        {
+            Border eventedR = (Border)sender;
+            eventedR.BorderBrush = Brushes.Chocolate;
+            eventedR.Background = Brushes.BlanchedAlmond;
+            TextBlock cellData = new TextBlock();
+            cellData.Text = "1"; // To Do : mine or number mines around
+            cellData.Margin = new Thickness(8, 4, 2, 2);
+            eventedR.Child = cellData;
+
+            border.MouseEnter -= mouse_in;
+            border.MouseLeave -= mouse_leave;
+            border.MouseDown -= mouse_down;
+
+
+
+        }
+    }
+}
--- Sapper/UI/MainWindow.xaml.cs
+++ Sapper/UI/MainWindow.xaml.cs
@@ -25,20 +25,78 @@ namespace Sapper
     public partial class MainWindow : Window
     {
         private AuthData _authData;
-
+        Rectangle re = new Rectangle();
+        Border bo = new Border();
+       
         public MainWindow()
         {
-            this.Hide();
-            AuthWindow authWindow = new AuthWindow();
-            authWindow.ShowDialog();
+           
+            
+            InitializeComponent();
+            bo.Height = 30;
+            bo.Width = 30;
+            //re.Stroke = Brushes.Gray;
+            //re.Name = "FieldCell";
+            
+            bo.BorderBrush = Brushes.Black;
+            bo.BorderThickness = new Thickness(2);
+            bo.Background = Brushes.Silver;
+            //MouseIn = Brushes.Black;
+            //re.Fill = Brushes.Silver;
+            bo.MouseEnter += mouse_in;
+            bo.MouseLeave += mouse_leave;
+            bo.MouseLeftButtonDown += mouse_down;
 
-            if (authWindow.authData == null)
-                Close();
-            else
-            {
-                _authData = authWindow.authData;
-                this.Show();
-            }
+            mainGrid.Children.Add(bo);
+            
+
+            
+
+            //this.Hide();
+            //AuthWindow authWindow = new AuthWindow();
+            //authWindow.ShowDialog();
+            //
+            //if (authWindow.authData == null)
+            //    Close();
+            //else
+            //{
+            //    _authData = authWindow.authData;
+            //    this.Show();
+            //}
+        }
+
+        private void mouse_in(object sender, EventArgs e)
+        {
+            Border eventedR = (Border)sender;
+            eventedR.BorderBrush = Brushes.Black;
+            
+            
+        }
+
+        private void mouse_leave(object sender, EventArgs e)
+        {
+            Border eventedR = (Border)sender;
+            eventedR.BorderBrush = Brushes.Gray;
+        }
+
+        private void mouse_down(object sender, EventArgs e)
+        {
+            Border eventedR = (Border)sender;
+            //eventedR.Fill = Brushes.LightCoral;
+            eventedR.BorderBrush = Brushes.Chocolate;
+            eventedR.Background = Brushes.BlanchedAlmond;
+            TextBlock t = new TextBlock();
+            t.Text = "1";
+            t.Margin = new Thickness(8, 4, 2, 2);
+            eventedR.Child = t;
+            
+
+            bo.MouseEnter -= mouse_in;
+            bo.MouseLeave -= mouse_leave;
+            bo.MouseDown -= mouse_down;
+
+
+
         }
     }
 }
