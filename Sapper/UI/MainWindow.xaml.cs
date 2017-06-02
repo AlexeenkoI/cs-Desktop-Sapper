66ee009ee63692ba230826935c20866563213ab8
barbeu <yuriy.barbeu@gmail.com>
2017-06-02 12:56:06 +0300

    Refactoring MainWindow.

--- Sapper/UI/MainWindow.xaml.cs
+++ Sapper/UI/MainWindow.xaml.cs
@@ -25,78 +25,27 @@ namespace Sapper
     public partial class MainWindow : Window
     {
         private AuthData _authData;
-        Rectangle re = new Rectangle();
-        Border bo = new Border();
        
         public MainWindow()
-        {
-           
-            
+        {                    
             InitializeComponent();
-            bo.Height = 30;
-            bo.Width = 30;
-            //re.Stroke = Brushes.Gray;
-            //re.Name = "FieldCell";
-            
-            bo.BorderBrush = Brushes.Black;
-            bo.BorderThickness = new Thickness(2);
-            bo.Background = Brushes.Silver;
-            //MouseIn = Brushes.Black;
-            //re.Fill = Brushes.Silver;
-            bo.MouseEnter += mouse_in;
-            bo.MouseLeave += mouse_leave;
-            bo.MouseLeftButtonDown += mouse_down;
-
-            mainGrid.Children.Add(bo);
-            
-
-            
-
-            //this.Hide();
-            //AuthWindow authWindow = new AuthWindow();
-            //authWindow.ShowDialog();
-            //
-            //if (authWindow.authData == null)
-            //    Close();
-            //else
-            //{
-            //    _authData = authWindow.authData;
-            //    this.Show();
-            //}
+            Auth();          
         }
 
-        private void mouse_in(object sender, EventArgs e)
+        private void Auth ()
         {
-            Border eventedR = (Border)sender;
-            eventedR.BorderBrush = Brushes.Black;
-            
-            
-        }
+            Hide();
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.ShowDialog();
 
-        private void mouse_leave(object sender, EventArgs e)
-        {
-            Border eventedR = (Border)sender;
-            eventedR.BorderBrush = Brushes.Gray;
-        }
-
-        private void mouse_down(object sender, EventArgs e)
-        {
-            Border eventedR = (Border)sender;
-            //eventedR.Fill = Brushes.LightCoral;
-            eventedR.BorderBrush = Brushes.Chocolate;
-            eventedR.Background = Brushes.BlanchedAlmond;
-            TextBlock t = new TextBlock();
-            t.Text = "1";
-            t.Margin = new Thickness(8, 4, 2, 2);
-            eventedR.Child = t;
-            
-
-            bo.MouseEnter -= mouse_in;
-            bo.MouseLeave -= mouse_leave;
-            bo.MouseDown -= mouse_down;
-
-
-
+            if (authWindow.authData == null)
+                Close();
+            else
+            {
+                _authData = authWindow.authData;
+                lb_nickName.Content = _authData.nickName;
+                Show();
+            }
         }
     }
 }
